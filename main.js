ff146dc919532883f4be96a503af544dff890adb
Kyle Rygiel <kbar41566@gmail.com>
2019-10-11 13:31:04 -0400

    added smooth scroll for CTA button

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
   $('.header').height($(window).height());
 })
+
+$('a[href*="#body"]').on('click', function(e) {
+  e.preventDefault()
+
+  $('html, body').animate(
+    {
+      scrollTop: $($(this).attr('href')).offset().top,
+    },
+    500,
+    'linear'
+  )
+})
